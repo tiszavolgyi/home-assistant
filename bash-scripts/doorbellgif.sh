64b11538d634c92f0962db05ab3ffc8a3d70a56a
Tiszavolgyi <tiszavolgyi@gmail.com>
2019-10-07 13:56:26 -0700

    doorbell image

---
+++ bash-scripts/doorbellgif.sh
@@ -0,0 +1,3 @@
+rm -rf a.gif
+#!/bin/bash
+convert -loop 0 -delay 100 /home/homeassistant/.homeassistant/www/tmp/frontdoor1.png /home/homeassistant/.homeassistant/www/tmp/frontdoor2.png /home/homeassistant/.homeassistant/www/tmp/frontdoor3.png /home/homeassistant/.homeassistant/www/tmp/a.gif
