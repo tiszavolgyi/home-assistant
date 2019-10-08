9a898517097815b07779c4b300ff230307ef479e
Peter Tiszavolgyi <tiszavolgyi@gmail.com>
2019-10-08 08:52:24 -0700

    doorbell shell command

--- bash-scripts/doorbellgif.sh
+++ bash-scripts/doorbellgif.sh
@@ -1,3 +1,3 @@
-rm -rf a.gif
+rm -rf /home/homeassistant/.homeassistant/www/tmp/a.gif
 #!/bin/bash
 convert -loop 0 -delay 100 /home/homeassistant/.homeassistant/www/tmp/frontdoor1.png /home/homeassistant/.homeassistant/www/tmp/frontdoor2.png /home/homeassistant/.homeassistant/www/tmp/frontdoor3.png /home/homeassistant/.homeassistant/www/tmp/a.gif
